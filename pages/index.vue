5e0dff332a5b95bbd6a40721b51938020f0f88b9
milos.papes <milos.papes@united.cloud>
2023-11-13 20:07:11 +0100

    landing page seo meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,30 @@
 <template>
+  <Head>
+    <Title>HotelSwiss: Hotel promoting platform</Title>
+    <Meta
+      name="description"
+      :content="`HotelSwiss: Promoting and connecting you with the best hotels Switzerland has to offer.`"
+    />
+    <!-- Open Graph -->
+    <Meta
+      property="og:url"
+      :content="`https://hotelswiss.ch`"
+    />
+    <Meta property="og:title" :content="`HotelSwiss: Hotel promoting platform`" />
+    <Meta
+      property="og:description"
+      :content="`HotelSwiss: Promoting and connecting you with the best hotels Switzerland has to offer.`"
+    />
+    <Meta property="og:image" :content="``" />
+    <!-- Twitter Card -->
+    <Meta name="twitter:card" content="summary_large_image" />
+    <Meta name="twitter:title" :content="`HotelSwiss: Hotel promoting platform`" />
+    <Meta
+      name="twitter:description"
+      :content="`HotelSwiss: Promoting and connecting you with the best hotels Switzerland has to offer.`"
+    />
+    <Meta name="twitter:image" :content="``" />
+  </Head>
   <div class="container mx-auto w-full">
     <PromoBlock class="promo-block" />
     <CategoryTabs />
