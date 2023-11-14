a76142fa6bc1e180c39dc45df9fda564ef98173a
Lemi <rajevic.mile6@gmail.com>
2023-11-14 23:54:02 +0100

    adds styles for category tabs

--- components/Common/BlockHeader.vue
+++ components/Common/BlockHeader.vue
@@ -1,11 +1,22 @@
 <template>
-  <h2 class="bold font-arial mb-3 w-full pb-3 text-2xl text-primary">
-    {{ title }}
-  </h2>
+  <div class="mb-3 flex w-full items-center justify-between">
+    <h2 class="bold font-arial w-full text-2xl text-primary">
+      {{ title }}
+    </h2>
+    <div v-if="link">
+      <NuxtLink
+        :to="`${link}`"
+        class="whitespace-nowrap text-primary underline hover:text-primary hover:no-underline"
+      >
+        View all
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 defineProps({
   title: String,
+  link: [String, Number],
 });
 </script>
