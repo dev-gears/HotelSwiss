eb72e1f078cc48ae9bda3080a9f8991a0d07c19c
milos.papes <milos.papes@united.cloud>
2023-11-28 00:39:59 +0100

    Lesser changes on landing page

--- components/CategoryTabs/TabContent.vue
+++ components/CategoryTabs/TabContent.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="flex flex-col items-center">
-    <CommonBlockHeader :title="name" :link="`category/${category}`" />
-    <SkeletonLoadersTabContentSkeleton v-if="isLoading" />
-    <div
-      class="flex grid-cols-2 flex-col gap-4 md:grid md:grid-cols-3 lg:grid-cols-4"
-      v-else-if="categorizedHotels.length"
-    >
-      <Card
-        v-for="hotel in categorizedHotels"
-        :key="hotel.id"
-        :hotel="hotel"
-        :showAmenities="true"
-      />
+    <div class="container mx-auto px-3">
+      <CommonBlockHeader :title="name" :link="`category/${category}`" />
+      <SkeletonLoadersTabContentSkeleton v-if="isLoading" />
+      <div
+        class="flex grid-cols-2 flex-col gap-4 md:grid md:grid-cols-3 lg:grid-cols-4"
+        v-else-if="categorizedHotels.length"
+      >
+        <Card
+          v-for="hotel in categorizedHotels"
+          :key="hotel.id"
+          :hotel="hotel"
+          :showAmenities="true"
+        />
+      </div>
+      <div v-else class="flex h-[400px] items-center justify-center">
+        {{ $t("TabContent.noHotelsFound") }}
+      </div>
     </div>
-    <div v-else class="flex h-[400px] items-center justify-center">
-      {{ $t("TabContent.noHotelsFound") }}
-    </div>
-
     <Button
       icon="pi pi-refresh"
       class="mx-auto mt-3 border px-3 py-2"
--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,8 +1,6 @@
 <template>
-  <header class="flex min-h-[40px] w-full bg-primary px-3 md:px-0">
-    <div
-      class="container mx-auto flex items-center justify-between p-3 md:px-0"
-    >
+  <header class="flex min-h-[40px] w-full bg-primary">
+    <div class="container mx-auto flex items-center justify-between p-3">
       <NuxtLink to="/">
         <img src="~/assets/images/minimal-logo.svg" alt="Website logo" />
       </NuxtLink>
